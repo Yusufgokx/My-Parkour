3ab9c8d02adb65a2f5db05b9ea51cebefec5f1ee
Yusufgokx <yusuf.fb.25@gmail.com>
2022-12-12 04:19:25 +0300

    ..
    ..

---
+++ Assets/Scripts/WeaponLock.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponLock : MonoBehaviour
+{
+    public LayerMask obstacleLayer;
+    public Vector3 offSet;
+
+    RaycastHit hit;
+ 
+    void Update()
+    {
+        if (Physics.Raycast(Camera.main.transform.position,Camera.main.transform.forward,out hit,obstacleLayer))
+        {
+            transform.LookAt(hit.point);
+            transform.rotation *= Quaternion.Euler(offSet);
+        }
+    }
+}
